db08bf6231b955c08bf652328c7e74f96f94764f
Dipesh <dipeshchand747@gmail.com>
2015-12-20 16:58:59 +0545

    Install and Configure MySQL for Django

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -37,24 +37,24 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
 		
-	def test_internationalization(self):
-    for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+    def test_internationalization(self):
+        for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
                                 ('ca', 'Benvingut a TaskBuster!')]:
-        activate(lang)
-        self.browser.get(self.get_full_url("home"))
-        h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertEqual(h1.text, h1_text)
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            h1 = self.browser.find_element_by_tag_name("h1")
+            self.assertEqual(h1.text, h1_text)
 		
-	def test_localization(self):
-    today = date.today()
-    for lang in ['en', 'ca']:
-        activate(lang)
-        self.browser.get(self.get_full_url("home"))
-        local_date = self.browser.find_element_by_id("local-date")
-        non_local_date = self.browser.find_element_by_id("non-local-date")
-        self.assertEqual(formats.date_format(today, use_l10n=True),
-                              local_date.text)
-        self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
+    def test_localization(self):
+        today = date.today()
+        for lang in ['en', 'ca']:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            local_date = self.browser.find_element_by_id("local-date")
+            non_local_date = self.browser.find_element_by_id("non-local-date")
+            self.assertEqual(formats.date_format(today, use_l10n=True),
+                                local_date.text)
+            self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
 	
     def test_time_zone(self):
         self.browser.get(self.get_full_url("home"))
--- taskbuster/settings/development.py
+++ taskbuster/settings/development.py
@@ -3,3 +3,14 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+	
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
